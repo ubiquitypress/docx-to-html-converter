71ed14c0e64e38772128085ea8e7732a05d17962
stuartjennings-up <stuart.jennings@ubiquitypress.com>
2016-07-15 11:20:50 +0100

    Added image handling capability

--- src/html_converter.py
+++ src/html_converter.py
@@ -1,12 +1,21 @@
 import os
 
 from docx2html import convert
+from shutil import copyfile
 
+
+def handle_image(image_id, relationship_dict):
+    image_path = relationship_dict[image_id]
+    _, filename = os.path.split(image_path)
+    destination_path = os.path.join('/tmp', filename)
+    copyfile(image_path, destination_path)
+
+    return 'file://%s' % destination_path
 
 def docx_converter():
 
     while True:
-        docx_path = raw_input('Please enter path to folder containing docx files:')
+        docx_path = raw_input('Please enter path to folder containing .docx files:')
         path_exists = os.path.exists(docx_path)
 
         if path_exists:
@@ -16,7 +25,7 @@ def docx_converter():
 
                     if ext == '.docx':
                         file_path = os.path.join(docx_path, file)
-                        html = convert(file_path)
+                        html = convert(file_path, image_handler=handle_image)
 
                         return html
 
@@ -24,3 +33,4 @@ def docx_converter():
             print 'Please enter a valid path.'
 
 docx_converter()
+
