8d9ed98e9111cf2d1c2047918348f4a442e603db
stuartjennings-up <stuart.jennings@ubiquitypress.com>
2016-07-28 12:52:16 +0100

    Specify write path and automatic names

--- src/html_converter.py
+++ src/html_converter.py
@@ -12,25 +12,39 @@ def handle_image(image_id, relationship_dict):
 
     return 'file://%s' % destination_path
 
+
 def docx_converter():
 
     while True:
         docx_path = raw_input('Please enter path to folder containing .docx files:')
-        path_exists = os.path.exists(docx_path)
+        docx_path_exists = os.path.exists(docx_path)
 
-        if path_exists:
-            for subdir, dirs, files in os.walk(docx_path):
-                for file in files:
-                    ext = os.path.splitext(file)[-1].lower()
+        if docx_path_exists:
+            write_path = raw_input('Please enter path to write html files to (conversion begins automatically): ')
+            write_path_exists = os.path.exists(write_path)
 
-                    if ext == '.docx':
-                        file_path = os.path.join(docx_path, file)
-                        html = convert(file_path, image_handler=handle_image)
+            if write_path_exists:
+                    for subdir, dirs, files in os.walk(docx_path):
+                        for file in files:
+                            ext = os.path.splitext(file)[-1].lower()
 
-                        return html
+                            if ext == '.docx':
+                                file_path = os.path.join(docx_path, file)
+                                html = convert(file_path, image_handler=handle_image)
+                                print 'Converting: ' + file
+
+                                # Give new html file same name as .docx original
+                                html_file_path = os.path.join(write_path, os.path.splitext(file)[0])
+                                html_file = open(html_file_path, 'w')
+                                html_file.write(html)
+                                html_file.close()
+
+            else:
+                print 'Please enter a valid path.'
+
 
         else:
             print 'Please enter a valid path.'
 
-docx_converter()
 
+docx_converter()
